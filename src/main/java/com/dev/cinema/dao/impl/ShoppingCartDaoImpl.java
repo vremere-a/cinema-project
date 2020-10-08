ae48405362948d362a2f60944c3e4d0bc1387b4a
Artem Vremere <vremere_a@ukr.net>
2020-10-08 12:24:18 +0300

    add ShoppingCartDao + Impl and TicketDao + Impl

---
+++ src/main/java/com/dev/cinema/dao/impl/ShoppingCartDaoImpl.java
@@ -0,0 +1,22 @@
+package com.dev.cinema.dao.impl;
+
+import com.dev.cinema.dao.ShoppingCartDao;
+import com.dev.cinema.model.ShoppingCart;
+import com.dev.cinema.model.User;
+
+public class ShoppingCartDaoImpl implements ShoppingCartDao {
+    @Override
+    public ShoppingCart add(ShoppingCart shoppingCart) {
+        return null;
+    }
+
+    @Override
+    public ShoppingCart getByUser(User user) {
+        return null;
+    }
+
+    @Override
+    public void update(ShoppingCart shoppingCart) {
+
+    }
+}
---
+++ src/main/java/com/dev/cinema/dao/impl/TicketDaoImpl.java
@@ -0,0 +1,11 @@
+package com.dev.cinema.dao.impl;
+
+import com.dev.cinema.dao.TicketDao;
+import com.dev.cinema.model.Ticket;
+
+public class TicketDaoImpl implements TicketDao {
+    @Override
+    public Ticket add(Ticket ticket) {
+        return null;
+    }
+}
