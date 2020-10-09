a96ed4da9edadf9eeeccacf272c4103ee95c608c
Artem Vremere <vremere_a@ukr.net>
2020-10-09 09:52:34 +0300

    mvn clean

--- src/main/java/com/dev/cinema/model/ShoppingCart.java
+++ src/main/java/com/dev/cinema/model/ShoppingCart.java
@@ -2,8 +2,6 @@ package com.dev.cinema.model;
 
 import java.util.List;
 import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.JoinTable;
