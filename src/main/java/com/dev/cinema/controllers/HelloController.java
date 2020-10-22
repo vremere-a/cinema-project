237760f9204ef380f2fcc8e922b57ca05c8d3a4c
Artem Vremere <vremere_a@ukr.net>
2020-10-22 11:35:58 +0300

    del log4j

--- src/main/java/com/dev/cinema/controllers/HelloController.java
+++ src/main/java/com/dev/cinema/controllers/HelloController.java
@@ -1,10 +1,8 @@
 package com.dev.cinema.controllers;
 
-import lombok.extern.log4j.Log4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@Log4j
 @RestController
 public class HelloController {
     @GetMapping("/hello")
