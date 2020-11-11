a4494e225d5123792c6930d27b583ac90dd3a172
Artem Vremere <vremere_a@ukr.net>
2020-11-11 16:50:05 +0200

    add user.setUserRole(Set.of(roleService.getRoleByName("USER")));

--- src/main/java/com/dev/cinema/security/implementations/AuthenticationServiceImpl.java
+++ src/main/java/com/dev/cinema/security/implementations/AuthenticationServiceImpl.java
@@ -1,27 +1,35 @@
 package com.dev.cinema.security.implementations;
 
+import com.dev.cinema.model.RoleName;
 import com.dev.cinema.model.User;
 import com.dev.cinema.security.interfaces.AuthenticationService;
+import com.dev.cinema.service.RoleService;
 import com.dev.cinema.service.ShoppingCartService;
 import com.dev.cinema.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.Set;
+
 @Service
 public class AuthenticationServiceImpl implements AuthenticationService {
     private final UserService userService;
     private final ShoppingCartService shoppingCartService;
+    private final RoleService roleService;
 
     @Autowired
     public AuthenticationServiceImpl(UserService userService,
-                                     ShoppingCartService shoppingCartService) {
+                                     ShoppingCartService shoppingCartService,
+                                     RoleService roleService) {
         this.userService = userService;
         this.shoppingCartService = shoppingCartService;
+        this.roleService = roleService;
     }
 
     @Override
     public User register(String email, String password) {
         User user = new User(email, password);
+        user.setUserRole(Set.of(roleService.getRoleByName("USER")));
         User userFromDB = userService.add(user);
         shoppingCartService.registerNewShoppingCart(userFromDB);
         return userFromDB;
